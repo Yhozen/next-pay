fca7d5a7c22aacb322e2bcbaad73fd86c5cf09ab
Gabriel Pérez <gabriel@garox.org>
2023-08-20 12:44:57 +0200

    fix dependencies

--- packages/next-pay/src/v2/next-pay.ts
+++ packages/next-pay/src/v2/next-pay.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { NextPayIntegration, PayCore, type Options } from 'next-pay-core'
 import { getBody, getURL, setHeaders } from './http.helpers'
