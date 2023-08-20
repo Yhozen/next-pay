e47a32e52b93a7e008336c91427882bbea03a1c9
Gabriel Pérez <gabriel@garox.org>
2023-08-20 12:47:55 +0200

    create handler and sdk

---
+++ apps/live-demo/src/pages/api/next-pay/[...next-pay].ts
@@ -0,0 +1,55 @@
+import {
+  createIntegrations,
+  defineNextPayConfig,
+  InferIntegrationType,
+  NextPay,
+} from "next-pay";
+import { createMercadoPagoIntegration } from "next-pay-core";
+
+const MercadoPagoIntegration = createMercadoPagoIntegration({
+  async accessToken(): Promise<string> {
+    return "YOUR_MERCADOPAGO_ACCESS_TOKEN";
+  },
+  back_urls: (id) => ({
+    success: `${process.env.NEXTAUTH_URL}/someroute/${id}/successful`,
+    failure: `${process.env.NEXTAUTH_URL}/someroute/${id}/rejected`,
+    pending: `${process.env.NEXTAUTH_URL}/someroute/${id}/pending`,
+  }),
+});
+
+const integrations = createIntegrations().add(MercadoPagoIntegration).compile();
+
+const nextPayConfig = defineNextPayConfig({
+  integrations,
+  name: "live-demo",
+  integrationConfig: {
+    async onApproved(integrationName, order) {
+      const invoiceId = order?.referenceId;
+
+      if (!invoiceId) return;
+
+      console.log({ invoiceId });
+    },
+    async onPending(integrationName, order) {
+      const invoiceId = order?.referenceId;
+
+      if (!invoiceId) return;
+
+      console.log({ invoiceId });
+    },
+
+    async onRejected(integrationName, order) {
+      const invoiceId = order?.referenceId;
+
+      if (!invoiceId) return;
+
+      console.log({ invoiceId });
+    },
+  },
+});
+
+export type IntegrationObject = InferIntegrationType<typeof nextPayConfig>;
+
+const { handler } = NextPay.create(nextPayConfig);
+
+export default handler;
