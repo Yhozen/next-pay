45f4f206e04732ace9ff76151cee958484eb82df
Gabriel P??rez <gabriel@garox.org>
2023-10-19 16:21:55 -0300

    fix env

--- apps/live-demo/src/env/server.ts
+++ apps/live-demo/src/env/server.ts
@@ -3,8 +3,8 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    MP_ACCESS_TOKEN: z.string().url(),
-    FINTOC_ACCESS_TOKEN: z.string().url(),
+    MP_ACCESS_TOKEN: z.string(),
+    FINTOC_ACCESS_TOKEN: z.string(),
     FINTOC_LINK_TOKEN: z.string().min(1),
   },
   experimental__runtimeEnv: {},
